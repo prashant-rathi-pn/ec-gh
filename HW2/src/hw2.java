8fc0515f8be9a195a0d0677261d5b408066684ae
prashantrathi <prashantrathi@192.168.1.100>
2018-01-25 23:24:29 +0530

    added source code file

---
+++ HW2/src/hw2.java
@@ -0,0 +1,13 @@
+
+public class hw2 {
+
+	public hw2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HW2:GIT is Cool");
+	}
+
+}
