85321f817b0ffaa784c5f02e96deb3bc3eeea5eb
prashantrathi <prashantrathi@192.168.1.100>
2018-01-25 23:26:13 +0530

    double

--- HW2/src/hw2.java
+++ HW2/src/hw2.java
@@ -8,6 +8,7 @@ public class hw2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HW2:GIT is Cool");
+		System.out.println("HW2:GIT is Cool");
 	}
 
 }
